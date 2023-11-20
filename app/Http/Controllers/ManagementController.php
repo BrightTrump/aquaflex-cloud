b740e373e4b6b970acb7a6046e57d8e2feabfa8c
samiecode <samiekeyz02@gmail.com>
2023-11-20 20:52:40 +0100

    Updated

--- app/Http/Controllers/ManagementController.php
+++ app/Http/Controllers/ManagementController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use App\Models\Product;
 use Illuminate\View\View;
+use App\Models\OrderStatus;
 use Illuminate\Http\Request;
+use App\Enums\OrderStatusEnum;
 use App\Services\FinancialSummaryService;
 
 class ManagementController extends Controller
 {
     protected $financialSummaryService;
 
-    public function __construct(FinancialSummaryService $financialSummaryService){
+    public function __construct(FinancialSummaryService $financialSummaryService)
+    {
         $this->financialSummaryService = $financialSummaryService;
     }
     public function index(): View
@@ -23,6 +27,6 @@ class ManagementController extends Controller
     {
         $products = Product::with('productItems')->get();
 
-        return view('admin.products', ['products'=> $products]);
+        return view('admin.products', ['products' => $products]);
     }
 }
