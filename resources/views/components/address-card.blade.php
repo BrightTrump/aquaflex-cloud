401331795220d5d05ddbd53a8ca28f8d55d92d68
samiecode <samiekeyz02@gmail.com>
2023-11-30 18:20:02 +0100

    Updated

--- resources/views/components/address-card.blade.php
+++ resources/views/components/address-card.blade.php
@@ -1,18 +1,15 @@
-<div class="grid grid-cols-1 xm:grid-cols-2 xm:p-10 p-4 gap-10">
-    @foreach ($userAddresses as $userAddress)
-        <div class="border border-gray-150 p-5 relative">
-            <a href="{{route('address.edit', $userAddress->address->id)}}" class="flex absolute right-5 top-5 text-md font-medium border border-primary-500 py-2 px-4 text-primary-500">Edit</a>
+<div class="border border-gray-150 p-5 relative">
+    <a href="{{ route('address.edit', $userAddress->address->id) }}"
+        class="flex absolute right-5 top-5 text-md font-medium border border-primary-500 py-2 px-4 text-primary-500">Edit</a>
 
-            <h1 class="font-medium text-2xl capitalize">{{$userAddress->user->name}}</h1>
-            <div class="text-xl">
-                <p>{{$userAddress->address->address_line1}}</p>
-                <p>{{$userAddress->address->address_line2}}</p>
-                <p>{{$userAddress->address->city}}, {{$userAddress->address->state}}.</p>
-                @if ($userAddress->is_default)
-                <p class="font-medium text-xl my-4 text-primary-500">Default Address</p>
-                @endif
-            </div>
+    <h1 class="font-medium text-2xl capitalize">{{ $userAddress->user->name }}</h1>
+    <div class="text-xl">
+        <p>{{ $userAddress->address->address_line1 }}</p>
+        <p>{{ $userAddress->address->address_line2 }}</p>
+        <p>{{ $userAddress->address->city }}, {{ $userAddress->address->state }}.</p>
+        @if ($userAddress->is_default)
+            <p class="font-medium text-xl my-4 text-primary-500">Default Address</p>
+        @endif
+    </div>
 
-        </div>
-    @endforeach
 </div>
--- resources/views/customer/address/index.blade.php
+++ resources/views/customer/address/index.blade.php
@@ -1,6 +1,10 @@
 <x-app-layout title="Address Book">
     <x-content>
         <h1 class="font-semibold text-3xl w-full border-b py-5 px-10">Address Book</h1>
-        <x-address-card :userAddresses="$userAddresses"/>
+        <div class="grid grid-cols-1 xm:grid-cols-2 xm:p-10 p-4 gap-10">
+            @foreach ($userAddresses as $userAddress)
+                <x-address-card :userAddress="$userAddress" />
+            @endforeach
+        </div>
     </x-content>
 </x-app-layout>
